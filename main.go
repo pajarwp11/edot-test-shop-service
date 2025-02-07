249d3e120a576e54d77b765dae60a343cfcb26aa
Pajar Wijaya Putra <pajar.putra.aux@flip.id>
2025-02-07 14:45:39 +0700

    add mysql connection

--- main.go
+++ main.go
@@ -4,11 +4,13 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"shop-service/db/mysql"
 
 	"github.com/gorilla/mux"
 )
 
 func main() {
+	mysql.Connect()
 	router := mux.NewRouter()
 
 	fmt.Println("server is running")
